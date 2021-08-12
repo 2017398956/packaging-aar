168012ef3814fa7e001411fbc6aa6224da354701
nfl <fuli.niu@zealcomm.cn>
2021-08-12 14:40:17 +0800

    update

---
+++ app/src/main/java/personal/nfl/packaging/aar/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package personal.nfl.packaging.aar.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
